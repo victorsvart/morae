8364911a2c10608a16515337eb8add95aca771e1
Victor Moraes <victormoraespt4@gmail.com>
2025-04-02 23:04:09 -0300

    new routing scheme

--- cmd/api/group.go
+++ cmd/api/group.go
@@ -1,42 +1,19 @@
 package main
 
 import (
-	"net/http"
-	"strings"
+	"log"
 )
 
 type Group struct {
-	router     *Router
-	prefix     string
+	RouteHandler
 	middleware []Middleware
 }
 
-func (g *Group) Use(middleware ...Middleware) {
-	g.middleware = append(g.middleware, middleware...)
-}
-
-func (g *Group) Handle(method, pattern string, handler http.HandlerFunc) {
-	fullPattern := g.prefix
-	if !strings.HasSuffix(g.prefix, "/") && !strings.HasPrefix(pattern, "/") && pattern != "" {
-		fullPattern = "/"
+func (g *Group) SubGroup(subGroupPrefix string) *SubGroup {
+	subGroup := &SubGroup{
+		RouteHandler: RouteHandler{router: g.router, prefix: g.prefix + subGroupPrefix},
 	}
 
-	fullPattern += pattern
-	g.router.Handle(method, fullPattern, handler)
-}
-
-func (g *Group) Get(pattern string, handler http.HandlerFunc) {
-	g.Handle(http.MethodGet, pattern, handler)
-}
-
-func (g *Group) Post(pattern string, handler http.HandlerFunc) {
-	g.Handle(http.MethodPost, pattern, handler)
-}
-
-func (g *Group) Put(pattern string, handler http.HandlerFunc) {
-	g.Handle(http.MethodPut, pattern, handler)
-}
-
-func (g *Group) Delete(pattern string, handler http.HandlerFunc) {
-	g.Handle(http.MethodDelete, pattern, handler)
+	log.Printf("Registered sub group: %s", subGroupPrefix)
+	return subGroup
 }
---
+++ cmd/api/routehandler.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+)
+
+type RouteHandler struct {
+	router     *Router
+	middleware []Middleware
+	prefix     string
+}
+
+func (rh *RouteHandler) Handle(method, pattern string, handler http.HandlerFunc) {
+	fullPattern := rh.prefix
+	if !strings.HasSuffix(rh.prefix, "/") && !strings.HasPrefix(pattern, "/") && pattern != "" {
+		fullPattern = "/"
+	}
+
+	fullPattern += pattern
+	rh.router.Handle(method, fullPattern, handler)
+}
+
+func (rh *RouteHandler) Use(mid ...Middleware) {
+	rh.middleware = append(rh.middleware, mid...)
+}
+
+func (r *RouteHandler) Get(pattern string, handler http.HandlerFunc) {
+	r.Handle(http.MethodGet, pattern, handler)
+}
+func (r *RouteHandler) Post(pattern string, handler http.HandlerFunc) {
+	r.Handle(http.MethodPost, pattern, handler)
+}
+func (r *RouteHandler) Put(pattern string, handler http.HandlerFunc) {
+	r.Handle(http.MethodPut, pattern, handler)
+}
+func (r *RouteHandler) Delete(pattern string, handler http.HandlerFunc) {
+	r.Handle(http.MethodDelete, pattern, handler)
+}
