0ebecee81b87c75dc8463849f278e5c2262b00af
Victor Moraes <victormoraespt4@gmail.com>
2025-04-05 08:24:36 -0300

    safer migration

--- cmd/migrate/migrations/000001_AddUsers.down.sql
+++ cmd/migrate/migrations/000001_AddUsers.down.sql
@@ -1,5 +1,39 @@
-DROP TRIGGER IF EXISTS trigger_update_timestamp ON mre_users;
+BEGIN;
 
-DROP FUNCTION IF EXISTS set_updated_at_column;
+-- Drop trigger if it exists
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1 
+        FROM pg_trigger 
+        WHERE tgname = 'trigger_update_timestamp'
+    ) THEN
+        DROP TRIGGER trigger_update_timestamp ON mre_users;
+    END IF;
+END $$;
 
-DROP TABLE IF EXISTS mre_users;
+-- Drop function if it exists
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1 
+        FROM pg_proc 
+        WHERE proname = 'set_updated_at_column'
+    ) THEN
+        DROP FUNCTION set_updated_at_column;
+    END IF;
+END $$;
+
+-- Drop table if it exists
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1 
+        FROM information_schema.tables 
+        WHERE table_name = 'mre_users'
+    ) THEN
+        DROP TABLE mre_users;
+    END IF;
+END $$;
+
+COMMIT;
